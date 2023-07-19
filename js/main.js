a5808deb3a7b16e3befc4aa9c22f00121061d738
Jelena <jelena.agiejewa@gmail.com>
2023-07-19 22:28:16 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
-const btnMobile = document.querySelector('.navbar-toggler');
-const navMobile = document.querySelector('.navbar-nav');
+// const btnMobile = document.querySelector('.navbar-toggler');
+// const navMobile = document.querySelector('.navbar-nav');
+
+// document.addEventListener('DOMContentLoaded', function () {
+// 	const nav = document.querySelector('.navbar');
+// 	const allNavItems = document.querySelectorAll('.nav-link');
+// 	const navList = document.querySelector('.navbar-collapse');
+
+// 	function addShadow() {
+// 		if (window.scrollY >= 130) {
+// 			nav.classList.add('shadow-bg');
+// 		} else {
+// 			nav.classList.remove('shadow-bg');
+// 		}
+// 	}
+
+// 	allNavItems.forEach(item => item.addEventListener('click', () => {
+// 		navList.classList.remove('show')
+// 	}));
+
+// 	window.addEventListener('scroll', addShadow);
+// });
+
+// function addShadowMobile() {
+// 	navMobile.classList.add('shadow-bg');
+// }
+
+// btnMobile.addEventListener('click', addShadowMobile);
+
 
 document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar');
@@ -7,22 +34,19 @@ document.addEventListener('DOMContentLoaded', function () {
 	const navList = document.querySelector('.navbar-collapse');
 
 	function addShadow() {
-		if (window.scrollY >= 130) {
+		if (window.scrollY >= 100) {
 			nav.classList.add('shadow-bg');
 		} else {
 			nav.classList.remove('shadow-bg');
 		}
 	}
 
-	allNavItems.forEach(item => item.addEventListener('click', () => {
-		navList.classList.remove('show')
-	}));
+	allNavItems.forEach(item =>
+		item.addEventListener('click', () => {
+			navList.classList.remove('show');
+		})
+	);
 
 	window.addEventListener('scroll', addShadow);
 });
 
-function addShadowMobile() {
-	navMobile.classList.add('shadow-bg');
-}
-
-btnMobile.addEventListener('click', addShadowMobile);
